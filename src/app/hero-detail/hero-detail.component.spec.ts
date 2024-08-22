8d581951ba159a0d1040eeb3f6a51ef6edf2a997
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-08-22 13:59:04 -0300

    FEAT basic async testing

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -51,4 +51,16 @@ describe('HeroDetailComponent', () => {
 
     expect(nativeElement.querySelector('h2')?.textContent).toContain('BATMAN');
   });
+
+  it('Should call updateHero whensave is called', (done) => {
+    mockHeroService.updateHero.and.returnValue(of(undefined));
+    fixture.detectChanges();
+
+    fixture.componentInstance.save();
+
+    setTimeout(() => {
+      expect(mockHeroService.updateHero).toHaveBeenCalled();
+      done();
+    }, 300);
+  });
 });
